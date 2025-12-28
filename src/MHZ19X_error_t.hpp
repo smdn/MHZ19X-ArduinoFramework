387563eb55cc1a29b7ed04379d15aa72a2ef3279
smdn <smdn@smdn.jp>
2025-12-28 19:57:15 +0900

    add comments

--- src/MHZ19X_error_t.hpp
+++ src/MHZ19X_error_t.hpp
@@ -7,10 +7,14 @@
 enum class MHZ19X_error_t {
   // Represents a value that is neither success nor failure.
   none = -1,
+
+  // Represents that the operation was successfully completed.
   success = 0,
+
   uart_write_failure,
   uart_read_timeout,
   uart_read_failure,
+
   returnvalue_startbyte_mismatch,
   returnvalue_commandbyte_mismatch,
   returnvalue_checksum_mismatch,
